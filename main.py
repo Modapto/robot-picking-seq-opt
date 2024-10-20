c73d504c39c5d79d37615147ba765cb6fdf1735c
kongiannakos <constantinosgiannakos@gmail.com>
2024-10-21 01:28:41 +0300

    New main python file. Ready to test it for postman.

---
+++ main.py
@@ -0,0 +1,232 @@
+import json
+import traceback
+from datetime import datetime
+from time import *
+import pika
+import sys
+import pandas as pd
+import numpy as np
+from instances_generator import *
+from parse_json import *
+from GraphCreation import *
+from heuristic_methods import *
+from reinforcement_learning import *
+from exact_final import *
+
+online = sys.argv[1]  # This argument will differentiate between local and remote runs
+
+def convert_to_native_types(data):
+    if isinstance(data, dict):
+        return {k: convert_to_native_types(v) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [convert_to_native_types(v) for v in data]
+    elif isinstance(data, pd.DataFrame):
+        return data.applymap(lambda x: int(x) if isinstance(x, (np.integer, np.int32, np.int64)) else x).to_dict()
+    elif isinstance(data, (np.integer, np.int32, np.int64)):
+        return int(data)
+    else:
+        return data
+
+def run_tsp(json_file_path=None, input_data=None, generate_new_instance=True):
+    total_time_start = int(time() * 1000)
+    total_loading_time = None  # Initialize here to avoid UnboundLocalError
+
+    # Step 1: Generate a new random JSON instance if generate_new_instance is True
+    if generate_new_instance:
+        random_json_input = generate_random_json_input()  # Assuming this function creates a valid JSON structure
+        json_file_path = "random_json_input.json"  # Path to save the new random JSON file
+        with open(json_file_path, 'w') as json_file:
+            json.dump(random_json_input, json_file, indent=4)
+        print(f"New random JSON input has been generated and saved to {json_file_path}.")
+
+        # Load this new input for further processing
+        input_data = random_json_input
+    else:
+        # Load input from file if it's not set to generate a new instance
+        if json_file_path:
+            with open(json_file_path, 'r') as f:
+                input_data = json.load(f)
+            print(f"Local JSON input has been loaded from {json_file_path}.")
+        elif input_data:
+            print("Remote input data received.")
+        else:
+            raise ValueError("Input data is required, either via JSON file or directly.")
+
+
+    # Parse the distance matrices
+    a_to_b_matrix = create_distance_matrices(input_data)
+    print(a_to_b_matrix)
+    print("Distance matrices created!")
+
+    # Create the bipartite graph
+    B, set_1, set_2 = create_directed_bipartite_graph(a_to_b_matrix)
+    print("Graph generated!")
+
+    start_node = input_data['data'].get('start_node', '0.0')
+    end_node = input_data['data'].get('end_node', '0.0.0')
+    method = input_data['data']['method']
+
+    if end_node not in set_2:
+        set_2.append(end_node)
+
+    results = {}
+    solution_time_start = int(time() * 1000)
+    simple_tour = None
+
+    if method == "nearest" or method == "all":
+        try:
+            print("Running Nearest Neighbor TSP...")
+            simple_tour = nearest_tsp(B, start_node, end_node, set_1, set_2)
+            simple_tour_cost, time_details = total_cost(B, simple_tour)
+            results["nearest"] = {
+                "tour": simple_tour,
+                "cost": simple_tour_cost,
+                "time_details": time_details,
+                "totalLoadingTime": simple_tour_cost
+            }
+            print(f"Nearest Neighbor TSP. Cost: {simple_tour_cost}")
+        except ValueError as e:
+            print(f"Error in nearest method: {e}")
+
+    # 2-opt Optimization
+    if method == "2-opt" or method == "all":
+        try:
+            print("Running 2-opt optimization...")
+            # if nearest_tsp was not run before, generate the tour first
+            if simple_tour is None:
+                print("Nearest neighbor tour not found, generating it for 2-opt...")
+                simple_tour = nearest_tsp(B, start_node, end_node, set_1, set_2)
+
+            optimized_tour = opt2(simple_tour, B, set_1, set_2, start_node, end_node)
+            optimized_tour_cost, time_details = total_cost(B, optimized_tour)
+            results["2-opt"] = {
+                "tour": optimized_tour,
+                "cost": optimized_tour_cost,
+                "time_details": time_details,
+                "totalLoadingTime": optimized_tour_cost
+            }
+            print(f"2-opt TSP completed. Cost: {optimized_tour_cost}")
+        except ValueError as e:
+            print(f"Error in 2-opt method: {e}")
+
+    if method == "q-learning" or method == "all":
+        try:
+            print("Running Q-Learning TSP...")
+            q_learning_tour = q_learning_tsp(B, start_node, end_node, set_1, set_2)
+            q_learning_tour_cost, time_details = total_cost(B, q_learning_tour)
+            results["q-learning"] = {
+                "tour": q_learning_tour,
+                "cost": q_learning_tour_cost,
+                "time_details": time_details,
+                "totalLoadingTime": q_learning_tour_cost
+            }
+            print(f"Q-Learning TSP completed. Cost: {q_learning_tour_cost}")
+        except ValueError as e:
+            print(f"Error in q-learning method: {e}")
+
+    if method == "exact" or method == "all":
+        try:
+            print("Running Exact Method TSP...")
+            exact_tour, exact_tour_cost, time_details = run_exact_tsp(input_data)
+
+            if exact_tour:
+                results["exact"] = {
+                    "tour": exact_tour,
+                    "cost": exact_tour_cost,
+                    "time_details": time_details,
+                    "totalLoadingTime": exact_tour_cost
+                }
+                print(f"Exact Method TSP completed. Cost: {exact_tour_cost}")
+        except ValueError as e:
+            print(f"Error in exact method: {e}")
+
+    if method == "all" and results:
+        min_cost_method = min(results, key=lambda k: results[k]["cost"])
+        results = {min_cost_method: results[min_cost_method]}
+        print(f"Selected method with minimum cost: {min_cost_method}")
+
+    end_time = int(time() * 1000)
+
+    picking_seq = []
+    for method, result in results.items():
+        if "time_details" in result:
+            picking_seq.append({"method": method})
+            picking_seq.extend(result["time_details"])
+            total_loading_time = result["totalLoadingTime"]
+
+    print("Picking Sequence:")
+    for pick in picking_seq:
+        print(pick)
+
+    output_data = {
+        "uuid": input_data['uuid'],
+        "produced_at": int(time() * 1000),
+        "data": {
+            "pickingSeq": picking_seq,
+            "totalLoadingTime": str(total_loading_time),
+            "solutionTime": (end_time - solution_time_start),
+            "totalTime": (end_time - total_time_start)
+        }
+    }
+
+    output_data = convert_to_native_types(output_data)
+
+    if json_file_path:
+        output_json_file_path = "output_local.json"
+        with open(output_json_file_path, 'w') as json_file:
+            json.dump(output_data, json_file, indent=4)
+        print(f"Local output saved to {output_json_file_path}")
+
+    return output_data
+
+def callback(ch, method, properties, body):
+    input_file = json.loads(body)
+    uuid = input_file['uuid']
+    data = input_file['data']
+
+    try:
+        print("%s: Job with uuid: %s received" % (datetime.now().strftime("%d/%m/%Y %H:%M:%S"), uuid))
+        input_postman_file_path = "input_postman.json"
+        output_postman_file_path = "output_postman.json"
+        #Run the TSP algorithm for the JSON input
+        output = process_json_data(input_file)
+
+        print("%s: Publishing results to queue." % (datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
+        output = json.dumps(output)
+        channel.basic_publish(exchange='opt-result', routing_key='robot-picking-seq', body=output)
+        print("%s: Job with uuid: %s completed" % (datetime.now().strftime("%d/%m/%Y %H:%M:%S"), uuid))
+    except Exception as e:
+        #print(Exception, e)
+        print(traceback.format_exc())
+        error_message = {"message": str(e)}
+        error_responce = {"uuid": uuid, "data": error_message, "produced_at": int(time() * 1000)}
+        error_responce = json.dumps(error_responce)
+        channel.basic_publish(exchange='opt-result', routing_key='robot-picking-seq', body=error_responce)
+        print("%s: Job with uuid: %s failed" % (datetime.now().strftime("%d/%m/%Y %H:%M:%S"), uuid))
+
+
+# Main Execution
+if online == "1":  # Remote mode with RabbitMQ
+    host = sys.argv[2]
+    port = sys.argv[3]
+    username = sys.argv[4]
+    password = sys.argv[5]
+    credentials = pika.PlainCredentials(username, password)
+    params = pika.ConnectionParameters(host, port, '/', credentials, heartbeat=1860, blocked_connection_timeout=930)
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+    channel.basic_consume(queue='robot-picking-seq_job', auto_ack=True, on_message_callback=callback)
+    channel.start_consuming()
+
+elif online == "0":  # Local mode with JSON file input
+    filename = sys.argv[2]
+    with open(filename) as f:
+        input = json.load(f)
+    print(datetime.now(), " - Data Received Successfully.")
+    uuid = input['uuid']
+    print(f"Process UUID: {input['uuid']}")
+    # print(input['data'])
+    input_data = input['data']
+    run_tsp(json_file_path=filename)
+
