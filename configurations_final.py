d4fe334e3a749b2f46354b609fa85013d7f32a08
kongiannakos <constantinosgiannakos@gmail.com>
2025-06-10 15:37:02 +0300

    local optimization change

--- configurations_final.py
+++ configurations_final.py
@@ -147,16 +147,16 @@ def generate_json_input(filtered_matrix):
         }
     }
     return random_json_input
-
-# Generate random configuration
-containers = copy.deepcopy(containers_template)
-kit_holders = copy.deepcopy(kit_holders_template)
-random_config = generate_random_configuration(containers, kit_holders)
-
-# Filter the distance matrix
-filtered_matrix = filter_distance_matrix(distance_matrix, random_config)
-# Generate the final JSON structure
-random_json_input = generate_json_input(filtered_matrix)
+#
+# # Generate random configuration
+# containers = copy.deepcopy(containers_template)
+# kit_holders = copy.deepcopy(kit_holders_template)
+# random_config = generate_random_configuration(containers, kit_holders)
+#
+# # Filter the distance matrix
+# filtered_matrix = filter_distance_matrix(distance_matrix, random_config)
+# # Generate the final JSON structure
+# random_json_input = generate_json_input(filtered_matrix)
 
 # # Save the results
 # with open("configurations_final.json", "w") as f:
