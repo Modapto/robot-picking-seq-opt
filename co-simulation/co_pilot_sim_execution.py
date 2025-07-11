e335f2aff0d1c40119d0e20a863ff3e6d3b92801
kongiannakos <constantinosgiannakos@gmail.com>
2025-07-11 22:26:25 +0300

    Co-simulation modifications

--- co-simulation/co_pilot_sim_execution.py
+++ co-simulation/co_pilot_sim_execution.py
@@ -1,6 +1,6 @@
 import json, pickle, base64
 from time import time
-from co_pilot_sim_preprocessing import *
+from co_pilot_sim_preprocessing import normalise_kh_sequences, containers_from_gr_sequence, gr_sequence_from_containers, generate_kh_configuration, shuffle_container_positions ,annotate_component_in_time_details ,calculate_full_sequence_cost
 
 def convert_to_native_types(data):
     if isinstance(data, dict):
@@ -19,7 +19,16 @@ def run_simulation(input_data=None):
         print("Remote input data received.")
         data = input_data["data"]
 
-        data["kh_sequences"] = normalise_kh_sequences(data.get("kh_sequences", []))
+        kh_seq = data.get("kh_sequences", [])
+        # If kh_seq is a dict, it likely needs translation/normalisation
+        if isinstance(kh_seq, dict):
+            data["kh_sequences"] = normalise_kh_sequences(kh_seq)
+        elif isinstance(kh_seq, list) and any(isinstance(k, dict) for k in kh_seq):
+            data["kh_sequences"] = normalise_kh_sequences(kh_seq)
+        else:
+            # Already translated (e.g., list of lists), no need to normalise again
+            data["kh_sequences"] = kh_seq
+
         if "gr_sequence" in data:                       # new style
             current_containers = containers_from_gr_sequence(
                 data["gr_sequence"], data["containers_template"]
